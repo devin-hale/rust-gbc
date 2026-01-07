3628e7e6d216890a3445f4c765e1dc44725b94e4
devin-hale <>
2026-01-06 21:08:45 -0600

    memory

---
+++ src/memory.rs
@@ -0,0 +1,50 @@
+use thiserror::Error;
+
+pub struct Memory {
+    m: [u8; 0xFFFF],
+}
+
+#[derive(Debug, Error)]
+pub enum MemoryError {
+    #[error("unknown memory error")]
+    Unknown,
+}
+
+impl Memory {
+    pub fn new() -> Memory {
+        Memory { m: [0u8; 0xFFFF] }
+    }
+
+    pub fn read(&self, addr: u16) -> Result<u8, MemoryError> {
+        Ok(self.m[addr as usize])
+    }
+
+    pub fn write(&mut self, addr: u16, data: u8) -> Result<(), MemoryError> {
+        self.m[addr as usize] = data;
+        Ok(())
+    }
+
+    pub fn write_word(&mut self, addr: u16, data: u16) -> Result<(), MemoryError> {
+        let low = (data & 0x00FF) as u8;
+        let high = ((data & 0xFF00) >> 8) as u8;
+        self.write(addr, low)?;
+        self.write(addr + 1, high)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn write_word() {
+        let mut mem = Memory::new();
+        let word = 0xFFEE;
+        let low = 0xEE;
+        let high = 0xFF;
+        mem.write_word(0, word).unwrap();
+        assert_eq!(mem.read(0x00).unwrap(), low);
+        assert_eq!(mem.read(0x01).unwrap(), high);
+    }
+}
