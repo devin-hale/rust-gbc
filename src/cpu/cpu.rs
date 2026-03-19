8a41ad34fef7c4614a4a73774b5906f98cace769
Devin Hale <devin@hale-mbp.local>
2026-03-19 18:54:50 -0500

    add xor a r

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -559,6 +559,7 @@ impl CPU {
             Op::Sub(s) => self.handle_sub(s),
             Op::SBC(s) => self.handle_sbc(s),
             Op::AND(r) => self.handle_and(r),
+            Op::XOR(r) => self.handle_xor(r),
             Op::Fetch(f) => self.handle_fetch(f)?,
             Op::Load(l) => self.handle_load(l)?,
             Op::Assert(r) => self.handle_assert(r),
@@ -1550,6 +1551,21 @@ impl CPU {
         self.a.write(result);
     }
 
+    fn handle_xor(&mut self, r: instr::Register) {
+        let val = self.src_register(r).unwrap() as u8;
+        let result = self.a.0 ^ val;
+        if result == 0 {
+            self.set_flag(Flag::Z);
+        } else {
+            self.reset_flag(Flag::Z);
+        }
+        self.reset_flag(Flag::N);
+        self.reset_flag(Flag::H);
+        self.reset_flag(Flag::C);
+
+        self.a.write(result);
+    }
+
     fn check_zero(&mut self, a: instr::Register, b: instr::Register, result: u16) -> bool {
         if a.is_byte() && b.is_byte() {
             return (result as u8) == 0;
@@ -2308,6 +2324,15 @@ mod test {
     fn test_and_a_r() {
         // AND A, R
         for i in 0x0..=0x7 {
+            let file = format!("a{:x}.json", i);
+            run_json_test(file);
+        }
+    }
+
+    #[test]
+    fn test_xor_a_r() {
+        // XOR A, R
+        for i in 0x8..=0xF {
             let file = format!("a{:x}.json", i);
             run_json_test(file);
         }
--- src/cpu/instr.rs
+++ src/cpu/instr.rs
@@ -155,6 +155,7 @@ impl Instruction {
             0x90..=0x97 => Instruction::sub_a_r(opcode),
             0x98..=0x9F => Instruction::sbc_a_r(opcode),
             0xA0..=0xA7 => Instruction::and(opcode),
+            0xA8..=0xAF => Instruction::xor(opcode),
             _ => todo!("opcode {}", opcode),
         }
     }
@@ -709,6 +710,40 @@ impl Instruction {
             }
         }
     }
+
+    fn xor(opcode: u8) -> Instruction {
+        let src = match opcode & 0xF {
+            0x0 | 0x8 => Register::B,
+            0x1 | 0x9 => Register::C,
+            0x2 | 0xA => Register::D,
+            0x3 | 0xB => Register::E,
+            0x4 | 0xC => Register::H,
+            0x5 | 0xD => Register::L,
+            0x6 | 0xE => Register::HL,
+            0x7 | 0xF => Register::A,
+            _ => panic!("invalid opcode {}", opcode),
+        };
+
+        if src == Register::HL {
+            Instruction {
+                cycles: (8, 0),
+                len: 1,
+                steps: vec![Step::with_ops(vec![
+                    Op::Assert(src),
+                    Op::XOR(Register::Memory),
+                ])],
+                ..Default::default()
+            }
+        } else {
+            Instruction {
+                cycles: (4, 0),
+                len: 1,
+                eager: true,
+                steps: vec![Step::with_ops(vec![Op::XOR(src)])],
+                ..Default::default()
+            }
+        }
+    }
 }
 
 impl Default for Instruction {
@@ -995,6 +1030,7 @@ pub enum Op {
     ADC(ADC),
     SBC(SBC),
     AND(Register),
+    XOR(Register),
     Fetch(Fetch),
     Assert(Register),
     AssertInc(Register),
